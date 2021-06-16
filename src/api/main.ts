ec7d83246e520ea81058bc5dfce9367f6cbbb70c
Andrew Lee <ajlee2006@gmail.com>
2021-06-16 15:56:38 +0800

    16 Jun 2021

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,5 +1,15 @@
-function main(): string {
-  return "Hello, world!";
+import {User} from "@/types/user";
+import {getRepos, getUser} from "@/api/api";
+
+async function main(): Promise<string> {
+  const name = "ajlee2006";
+  const user = await getUser(name);
+  const repos = await getRepos(name);
+  const starredRepos = repos.filter(repo => {
+    return repo.stargazers_count > 0;
+  });
+  const repoNames = repos.map(repo => repo.name).join(", ");
+  return `${user.login} has ${user.followers} followers, ${starredRepos.length} starred repos and ${repos.length} repos: ${repoNames}`;
 }
 
 export default {
