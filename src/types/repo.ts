b3eb4d19a3c5a8c0c487045fac891d55571863af
Andrew Lee <ajlee2006@gmail.com>
2021-06-21 15:13:43 +0800

    First part of day 2

--- src/types/repo.ts
+++ src/types/repo.ts
@@ -1,5 +1,6 @@
 export class Repo {
   name!: string
+  full_name!: string
   stargazers_count!: number
   size!: number
 }
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -16,9 +16,21 @@
           label="Name"
         >
         </v-text-field>
-        <div>
-          Output of <code>main</code>: {{ mainOut }}
-        </div>
+<!--        <div>-->
+<!--          Output of <code>main</code>: {{ mainOut }}-->
+<!--        </div>-->
+        <v-data-table
+          :items="repos"
+          :headers="headers"
+          >
+          <template v-slot:item.name="{ item }">
+            <a :href="`https://github.com/${item.full_name}`" target="_blank">{{item.name}}</a>
+          </template>
+          <template v-slot:item.stargazers_count="{ item }">
+            {{item.stargazers_count}}
+            <v-icon> {{ getIcon(item.stargazers_count) }}</v-icon>
+          </template>
+        </v-data-table>
       </div>
     </v-card>
   </v-container>
@@ -27,17 +39,39 @@
 <script lang="ts">
 import Vue from "vue";
 import api from "@/api/main";
+import {Repo} from "@/types/repo";
+import {DataTableHeader} from "vuetify";
+import {getRepos} from "@/api/api";
 
 export default Vue.extend({
   name: "Main",
   data: function () {
     return {
       name: "",
-      mainOut: ""
+      mainOut: "",
+      repos: [] as Repo[],
+      headers: [{
+        text: "Name",
+        value: "name"
+      }, {
+        text: "Stars",
+        value: "stargazers_count"
+      }, {
+        text: "Size",
+        value: "size"
+      }] as DataTableHeader[]
     };
   },
+  methods:{
+    getIcon(stars: number): string {
+      if(stars > 2){
+        return "mdi-emoticon-happy-outline";
+      } return "mdi-emoticon-neutral-outline";
+    }
+  },
   async mounted() {
-    this.mainOut = await api.main();
+    // this.mainOut = await api.main();
+    this.repos = await getRepos("ajlee2006");
   },
 });
 </script>
